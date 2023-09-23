dd719ab491e1cde8a0186b4b29b2459079331d3e
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2023-09-22 22:20:03 -0300

    defining addition, multiplication, exponentiation, double, fibonacci and factorial recursively

--- Nat.hs
+++ Nat.hs
@@ -1,4 +1,41 @@
 module Nat where
 
+import Prelude hiding
+    ( (+)
+    , (*)
+    , (^)
+    )
+
 data Nat = O | S Nat
     deriving ( Eq , Show )
+
+-- Addition
+(+) :: Nat -> Nat -> Nat
+(+) n O = n
+(+) n (S m) = S((+) n m)
+
+-- Multiplication
+(*) :: Nat -> Nat -> Nat
+(*) n O = O
+(*) n (S m) = (+) ((*) n m) n
+
+-- Exponentiation
+(^) :: Nat -> Nat -> Nat
+(^) n O = (S O)
+(^) n (S m) = (*) ((^) n m) n
+
+-- Double
+double :: Nat -> Nat
+double O = O
+double (S n)  = (S (S (double n)))
+
+-- Fibonacci
+fib :: Nat -> Nat
+fib O = O
+fib (S O) = (S O)
+fib (S (S n)) = (+) (fib n) (fib (S n))
+
+-- Factorial
+fact :: Nat -> Nat
+fact O = (S O)
+fact (S n) = (*) (S n) (fact n)

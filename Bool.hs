6504431aa638f336f6f61d76c7f88ff8b3c0d95d
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2023-10-10 23:09:31 -0300

    defining bool datatype and functions leq, ev, od, isMul3, isZero

---
+++ Bool.hs
@@ -0,0 +1,33 @@
+module Bool where
+
+import Prelude hiding (Bool, True, False, not)
+import Nat
+
+data Bool = False | True
+    deriving ( Eq, Show )
+
+not :: Bool -> Bool
+not True = False
+not False = True
+
+leq :: Nat -> Nat -> Bool
+leq O _ = True
+leq _ O = False
+leq (S n) (S m) = leq n m
+
+ev :: Nat -> Bool
+ev O = True
+ev (S O) = False
+ev (S (S n)) = ev n
+
+od :: Nat -> Bool
+od n = not (ev n)
+
+isMul3 :: Nat -> Bool
+isMul3 O = True
+isMul3 (S (S (S n))) = isMul3 n
+isMul3 _ = False
+
+isZero :: Nat -> Bool
+isZero O = True
+isZero _ = False
